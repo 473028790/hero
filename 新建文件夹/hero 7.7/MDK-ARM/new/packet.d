32429006dfe725cb94b36ae564b77a8f84c40324
“473028790” <473028790@qq.com>
2022-07-11 14:00:50 +0800

    改了拨盘

---
+++ 新建文件夹/hero 7.7/MDK-ARM/new/packet.d
@@ -0,0 +1,6 @@
+new\packet.o: user\packet.c
+new\packet.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+new\packet.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+new\packet.o: user\packet.h
+new\packet.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+new\packet.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
---
+++ 新建文件夹/hero 7.7/MDK-ARM/user/task.h
@@ -0,0 +1,19 @@
+#ifndef _TASK_H_
+#define _TASK_H_
+
+
+void Offline_task(void);
+void CAN1_send(void);
+void CAN2_send(void);
+void CAN1_super(void);
+
+
+
+
+
+
+
+
+
+#endif
+
